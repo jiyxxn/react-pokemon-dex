835dd60e47675e2c20bc7023916964c909e7f3e5
jinew <jiyunk@naver.com>
2025-02-04 16:31:15 +0900

    ♻️ Refactor : 유효성 검증 로직과 UI 로직 분리

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,7 +1,5 @@
-import { toast } from 'react-toastify';
 export const validateMaximum = (prevData, max) => {
   if (prevData.length >= max) {
-    // toast.warning('더 이상 데려갈 수 없습니다.');
     return false;
   }
   return true;
@@ -13,7 +11,6 @@ export const validateExisted = (prevData, selectedId) => {
   });
 
   if (isExisted.length > 0) {
-    alert('이미 데려간 포켓몬입니다.');
     return false;
   }
   return true;
