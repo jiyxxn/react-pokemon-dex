cc5126b6378f58af4f92776df405eb4d4d676e1b
jinew <jiyunk@naver.com>
2025-02-04 12:40:43 +0900

    ✨ Update : 토스트 알럿 패키지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css';
 import Router from './shared/Router';
+import { ToastContainer, Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
       <Router />
+      <ToastContainer
+        position="top-center"
+        closeButton={false}
+        autoClose={4000}
+        theme="light"
+        transition={Bounce}
+      />
     </>
   );
 }
