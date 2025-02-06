243bba7c8716071563dd15a7f4bec47a289538e3
jinew <jiyunk@naver.com>
2025-02-06 10:49:15 +0900

    ✨ Update : 유효성 검증 로직 및 주석 추가

--- src/hooks/usePokemonActions.js
+++ src/hooks/usePokemonActions.js
@@ -1,18 +1,24 @@
 import { useDispatch } from 'react-redux';
 import { deletePokemon, registerPokemon } from '../store/pokemonSlice';
-import toggleBtnType from '../utils/toggleBtnType';
 
 const usePokemonActions = () => {
   const dispatch = useDispatch();
 
+  // * 포켓몬 카드에서 선택/해제 액션을 처리하는 이벤트 핸들러
   const handleSelectedPokemon = (e) => {
-    e.stopPropagation();
+    e.stopPropagation(); // 이벤트 버블링 방지
 
     const toggleBtn = e.target.getAttribute('data-toggle');
+    if (!toggleBtn) return; // 유효하지 않은 버튼 클릭 방지
+
     const selectedId = Number(
       e.target.closest('.poke-card').getAttribute('data-id')
     );
+    /* 암시적 형변환 없이 정확한 NaN 판별
+        - e.g. isNaN(""), isNaN("null") 은 값이 0으로 변환되면서 false가 반환됨*/
+    if (Number.isNaN(selectedId)) return;
 
+    // data-toggle 값에 따라 포켓몬 등록 또는 삭제 실행
     toggleBtn === 'register'
       ? dispatch(registerPokemon(selectedId))
       : dispatch(deletePokemon(selectedId));
--- src/store/pokemonSlice.js
+++ src/store/pokemonSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import POKEMON_DATA from '../mocks/pokemonData';
-import { validateExisted, validateMaximum } from '../utils/validate';
+import { validateNotExisted, validateMaximum } from '../utils/validate';
 import { MAX_POKEMON } from '../shared/constants';
 import { toast } from 'react-toastify';
 
@@ -13,29 +13,34 @@ const pokemonSlice = createSlice({
   name: 'pokemon',
   initialState,
   reducers: {
+    // * 포켓몬 등록
     registerPokemon: (state, action) => {
+      const selectedPokemon = state.pokemonData.find(
+        (pokemon) => pokemon.id === action.payload
+      );
+
+      if (!selectedPokemon) return;
+
       if (!validateMaximum(state.selectedPokemon, MAX_POKEMON)) {
         toast.error('더 이상 데려갈 수 없습니다.');
         return;
       }
-      if (!validateExisted(state.selectedPokemon, action.payload)) {
+      if (!validateNotExisted(state.selectedPokemon, action.payload)) {
         toast.error('이미 데려간 포켓몬입니다.');
         return;
       }
 
-      const selectedPokemon = state.pokemonData.find(
-        (pokemon) => pokemon.id === action.payload
-      );
-      if (selectedPokemon) {
-        toast.success(`${selectedPokemon.korean_name}, 전투 준비 완료 ⚔️`);
-        state.selectedPokemon.push(selectedPokemon);
-      }
+      toast.success(`${selectedPokemon.korean_name}, 전투 준비 완료 ⚔️`);
+      state.selectedPokemon.push(selectedPokemon);
     },
 
+    // * 포켓몬 삭제
     deletePokemon: (state, action) => {
       const removedPokemon = state.selectedPokemon.find(
         (pokemon) => pokemon.id === action.payload
       );
+
+      if (!removedPokemon) return;
 
       state.selectedPokemon = state.selectedPokemon.filter(
         (pokemon) => pokemon.id !== action.payload
