b114dc67c187410c821e7dc28993c258a6f750c0
jinew <jiyunk@naver.com>
2025-02-06 20:34:02 +0900

    ♻️ Refactor : 글로벌 스타일 분리 및 리팩토링

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,78 +5,65 @@ import PokemonCard from './PokemonCard.jsx';
 import { useSelector } from 'react-redux';
 import usePokemonActions from '../hooks/usePokemonActions.js';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { createGlobalStyle } from 'styled-components';
+import GlobalStyles from '../styles/globalStyles.js';
 
 const Dashboard = () => {
   const pokemon = useSelector((state) => state.pokemon.selectedPokemon);
   const { handleSelectedPokemon } = usePokemonActions();
 
-  const renderSelectedPokemonList = () => {
-    const renderList = [];
-    for (let i = 0; i < MAX_POKEMON; i++) {
-      if (pokemon[i]) {
-        renderList.push(
-          <SwiperSlide key={i}>
+  const renderSelectedPokemonList = () =>
+    Array.from({ length: MAX_POKEMON }, (_, i) => {
+      const selected = pokemon[i];
+
+      return (
+        <SwiperSlide key={i}>
+          {selected ? (
             <PokemonCard
-              key={i}
-              img={pokemon[i].img_url}
-              name={pokemon[i].korean_name}
-              number={pokemon[i].id}
-              types={pokemon[i].types}
+              img={selected.img_url}
+              name={selected.korean_name}
+              number={selected.id}
+              types={selected.types}
               button="delete"
-              arrivedAt={`/detail?dexNum=${pokemon[i].id}`}
+              arrivedAt={`/detail?dexNum=${selected.id}`}
               onPokemonSelect={handleSelectedPokemon}
             />
-          </SwiperSlide>
-        );
-      } else {
-        renderList.push(
-          <SwiperSlide key={i}>
+          ) : (
             <EmptyCard />
-          </SwiperSlide>
-        );
-      }
-    }
-    return renderList;
-  };
+          )}
+        </SwiperSlide>
+      );
+    });
 
   return (
     <>
       <GlobalStyles />
-      <BoardWrapper
-        $padding="40px"
-        $backgroundColor="#fdf7e5"
-        $shadow="0px 0px 10px 2px #e2c6ac;"
-        $overflow="hidden"
-      >
-        <h2>Battle Partners 🚀</h2>
-        <Swiper
-          spaceBetween={20}
-          slidesPerView={2}
-          observer={true} // Swiper 크기 변화를 감지
-          observeParents={true} // 부모 요소의 크기 변화를 감지
-          breakpoints={{
-            1280: { slidesPerView: 6 },
-            1024: { slidesPerView: 5 },
-            768: { slidesPerView: 4 },
-            540: { slidesPerView: 3 },
-            480: { slidesPerView: 2 },
-          }}
-        >
-          {renderSelectedPokemonList()}
-        </Swiper>
+      <BoardWrapper {...boardStyle}>
+        <div>
+          <h2>Battle Partners 🚀</h2>
+          <Swiper
+            spaceBetween={20}
+            slidesPerView={2}
+            breakpoints={{
+              1280: { slidesPerView: 6 },
+              1024: { slidesPerView: 5 },
+              768: { slidesPerView: 4 },
+              540: { slidesPerView: 3 },
+              480: { slidesPerView: 2 },
+            }}
+          >
+            {renderSelectedPokemonList()}
+          </Swiper>
+        </div>
       </BoardWrapper>
     </>
   );
 };
 
-const GlobalStyles = createGlobalStyle`
-  .swiper  {
-    overflow: visible;
-  }
-  .swiper-wrapper {
-    align-items: center;
-  }
-`;
+const boardStyle = {
+  $padding: '40px',
+  $backgroundColor: '#fdf7e5',
+  $boxShadow: '0px 0px 10px 2px #e2c6ac',
+  $overflow: 'hidden',
+};
 
 export default Dashboard;
