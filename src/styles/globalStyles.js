16f56ddb65c11ae82b8b3586c418321e34f30498
jinew <jiyunk@naver.com>
2025-02-06 20:33:12 +0900

    🎉 Add : globalStyles

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  .swiper  {
+    overflow: visible;
+  }
+  .swiper-wrapper {
+    align-items: center;
+  }
+`;
+
+export default GlobalStyles;
